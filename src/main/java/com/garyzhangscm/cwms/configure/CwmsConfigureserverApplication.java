7de63a3e313be057b2b407dc5b98cdfcf9353242
Gary <gzhang1999@gmail.com>
2019-07-15 15:18:30 -0700

    initial commit

---
+++ src/main/java/com/garyzhangscm/cwms/configure/CwmsConfigureserverApplication.java
@@ -0,0 +1,13 @@
+package com.garyzhangscm.cwms.configure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwmsConfigureserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwmsConfigureserverApplication.class, args);
+	}
+
+}
